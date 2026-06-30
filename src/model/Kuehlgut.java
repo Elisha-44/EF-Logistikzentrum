860b39d980fc3dbfc0401137f27954d469db8df5
mahoelis <mahoelis@kkg-do.eu>
2026-06-30 13:13:58 +0200

    Kuehlgut machen

--- src/model/Kuehlgut.java
+++ src/model/Kuehlgut.java
@@ -1,8 +1,36 @@
 package model;
 
 public class Kuehlgut extends Frachtgut{
+    private double aktuelleTemp;
+    private double minimalTemp;
+    private double maximalTemp;
+    private boolean tempNichtEingehalten;
 
-    public Kuehlgut(String bez){
-        super(bez);
+    public Kuehlgut(String bez, double aktT, double minT, double maxT){
+        super(Logistikzentrum(bez));
+        //
+        aktuelleTemp = aktT;
+        minimalTemp = minT;
+        maximalTemp = maxT;
+        tempNichtEingehalten = false;
+    }
+
+    public boolean kannKuehlen(){
+        return aktuelleTemp;
+    }
+
+    public boolean gibMinimalTemp(){
+        return minimalTemp;
+        //
+    }
+
+    public boolean gibMaximalTemp(){
+        return maximalTemp;
+    }
+
+    public void pruefeTemp(){
+        if(aktuelleTemp >= maximalTemp && aktuelleTemp <= minimalTemp){
+
+        }
     }
 }
